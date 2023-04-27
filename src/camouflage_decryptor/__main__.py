cedccbb2dff94ec61c56f79199aa813de1a22b55
anjomro <github@anjomro.de>
2023-04-27 12:47:44 +0200

    Initial Commit

---
+++ src/camouflage_decryptor/__main__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2023-present anjomro <py@anjomro.de>
+#
+# SPDX-License-Identifier: EUPL-1.2
+import sys
+
+if __name__ == "__main__":
+    from camouflage_decryptor.cli import camouflage_decryptor
+
+    sys.exit(camouflage_decryptor())
---
+++ src/camouflage_decryptor/cli/__init__.py
@@ -0,0 +1,12 @@
+# SPDX-FileCopyrightText: 2023-present anjomro <py@anjomro.de>
+#
+# SPDX-License-Identifier: EUPL-1.2
+import click
+
+from camouflage_decryptor.__about__ import __version__
+
+
+@click.group(context_settings={"help_option_names": ["-h", "--help"]}, invoke_without_command=True)
+@click.version_option(version=__version__, prog_name="camouflage-decryptor")
+def camouflage_decryptor():
+    click.echo("Hello world!")
